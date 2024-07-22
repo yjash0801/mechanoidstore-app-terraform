af21f020a03067d73b0e32188332ef971246a30d
yjash0801 <y.jashwanth5668@gmail.com>
2024-07-22 16:19:09 -0400

    Final commits

---
+++ variables.tf
@@ -0,0 +1,47 @@
+variable "project_name" {
+    # default = "mechanoidstore"
+}
+
+variable "environment" {
+    # default = "dev"
+}
+
+variable "common_tags" {
+    # default = {
+    #     Name = "mechanoidstore"
+    #     Environment = "dev"
+    #     Terraform = "true"
+    # }
+}
+
+variable "tags" {
+
+}
+
+variable "dns" {
+    #default = "mechanoidstore.online"
+}
+
+variable "vpc_id" {
+  
+}
+
+variable "component_sg_id" {
+  
+}
+
+variable "private_subnet_ids" {
+  
+}
+
+variable "iam_instance_profile" {
+  
+}
+
+variable "priority" {
+  
+}
+
+variable "listener_arn" {
+  
+}
